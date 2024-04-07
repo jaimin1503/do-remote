6a8c3dc37d97e6f5430727a8eb1b45514578ea44
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-04-07 20:20:41 +0530

    cookies setup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -208,6 +208,8 @@ export const login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
     };
 
     // Set token in a cookie
