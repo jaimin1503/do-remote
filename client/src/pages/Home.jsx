5c493db9d0b15841856258a5fc6636bf04d0e69a
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-04-20 08:04:28 +0530

    home page styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navebar from "../components/Navebar";
 import home1 from "./assets/home1.png";
 import { Link } from "react-router-dom";
+import "./Styles.css";
 
 export default function Home() {
   return (
@@ -9,16 +10,16 @@ export default function Home() {
       <div className="row1 sm:flex sm:justify-center sm:items-center">
         <div className=" mt-4 flex flex-col h-full items-center sm:items-start m-4">
           <div className=" my-4">
-            <h1 className="text-5xl max-w-xs text-blue-500 font-bold">
+            <h1 className="text-5xl max-w-xs header-gradient font-bold">
               Welcome to Do-Remote
             </h1>
-            <h1 className=" text-3xl sm:text-4xl text-gray-400 my-2 font-medium mt-4">
+            <h1 className=" text-3xl sm:text-4xl sub-header-gradient my-2 font-medium mt-4">
               Explore the sea of Talent,
             </h1>
-            <h1 className=" text-3xl sm:text-4xl text-gray-400 my-2 font-medium">
+            <h1 className=" text-3xl sm:text-4xl sub-header-gradient my-2 font-medium">
               Find the work that suits you.
             </h1>
-            <div className=" px-5 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full mt-4 w-fit font-medium">
+            <div className=" px-5 py-2 cursor-pointer bg-blue-500 hover:bg-blue-600 text-white rounded-full mt-4 w-fit font-medium">
               <Link to={`/welcome`}>Get Started</Link>
             </div>
           </div>
@@ -32,9 +33,7 @@ export default function Home() {
         </div>
       </div>
       <div className="row2">
-        <div className="image">
-          
-        </div>
+        <div className="image"></div>
       </div>
     </div>
   );
