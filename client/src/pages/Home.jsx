d87fc2b44a1eda99ea25a222640e5ec37fad2951
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-04-20 18:31:57 +0530

    styling home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Navebar from "../components/Navebar";
 import home1 from "./assets/home1.png";
 import { Link } from "react-router-dom";
+import img2 from "./assets/img2.jpg";
 import "./Styles.css";
+import AssignmentTurnedInOutlinedIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
+import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 
 export default function Home() {
   return (
@@ -19,9 +23,12 @@ export default function Home() {
             <h1 className=" text-3xl sm:text-4xl sub-header-gradient my-2 font-medium">
               Find the work that suits you.
             </h1>
-            <div className=" px-5 py-2 cursor-pointer bg-blue-500 hover:bg-blue-600 text-white rounded-full mt-4 w-fit font-medium">
-              <Link to={`/welcome`}>Get Started</Link>
-            </div>
+            <button
+              onClick={() => window.location.replace(`/welcome`)}
+              className=" px-5 py-2 cursor-pointer bg-blue-500 hover:bg-blue-600 text-white rounded-full mt-4 w-fit font-medium"
+            >
+              Get Started
+            </button>
           </div>
         </div>
         <div className=" hidden md:block">
@@ -32,8 +39,105 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="row2">
-        <div className="image"></div>
+      <div className="row2 m-4 flex flex-col md:flex-row justify-evenly">
+        <div className="image">
+          <img
+            className=" h-[50vh] w-full md:w-[50vw] rounded-2xl object-cover"
+            src={img2}
+            alt="image"
+          />
+        </div>
+        <div className=" md:w-1/2 p-4">
+          <h1 className="text-3xl font-bold">Why Do-Remote?</h1>
+
+          <div className="my-4">
+            <div className=" flex items-start ">
+              <div>
+                <AssignmentTurnedInOutlinedIcon className="text-3xl text-blue-500" />
+              </div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">No cost to join.</h1>
+                <h1 className=" text-sm text-gray-600">
+                  Register and browse professionals, explore projects, or even
+                  book a consultation.
+                </h1>
+              </div>
+            </div>
+            <div className=" flex items-start mt-3 ">
+              <div>
+                <PushPinOutlinedIcon className="text-3xl text-blue-500" />
+              </div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">
+                  Post a job and hire top talent
+                </h1>
+                <h1 className=" text-sm text-gray-600">
+                  Finding talent doesn't have to be a chore. Post a job or we
+                  can search for you!
+                </h1>
+              </div>
+            </div>
+            <div className=" flex items-start mt-3">
+              <div>
+                <StarBorderOutlinedIcon className="text-3xl text-blue-500" />
+              </div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">
+                  Work with the best—without breaking the bank
+                </h1>
+                <h1 className=" text-sm text-gray-600">
+                  Upwork makes it affordable to up your work and take advantage
+                  of low transaction rates.
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row3 m-4 flex flex-col md:flex-row justify-evenly">
+        <div className=" md:w-1/2 p-4">
+          <h1 className="text-3xl font-bold">How it works?</h1>
+
+          <div className="my-4">
+            <div className=" flex items-start ">
+              <div className="text-3xl text-blue-500">1</div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">Post a job</h1>
+                <h1 className=" text-sm text-gray-600">
+                  Tell us about your project. Upwork connects you with top
+                  talent and agencies around the world, or near you.
+                </h1>
+              </div>
+            </div>
+            <div className=" flex items-start mt-3 ">
+              <div className="text-3xl text-blue-500">2</div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">Hire</h1>
+                <h1 className=" text-sm text-gray-600">
+                  Upwork connects you with top talent and agencies around the
+                  world, or near you.
+                </h1>
+              </div>
+            </div>
+            <div className=" flex items-start mt-3">
+              <div className="text-3xl text-blue-500">3</div>
+              <div className=" pl-2">
+                <h1 className=" text-lg font-medium">Work</h1>
+                <h1 className=" text-sm text-gray-600">
+                  Use Upwork to chat or video call, share files, and track
+                  project milestones from your desktop or mobile.
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img
+            className=" h-[50vh] w-full md:w-[50vw] rounded-2xl object-cover"
+            src={img2}
+            alt="image"
+          />
+        </div>
       </div>
     </div>
   );
