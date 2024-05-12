656339a0ae4142ee1f6a8a37d9aa89853acdc2e9
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-05-12 09:18:14 +0530

    updated Ai page

--- client/src/pages/AIDS.jsx
+++ client/src/pages/AIDS.jsx
@@ -1,11 +1,19 @@
-import Navebar from "../components/Navebar";
+import aiImg from "./assets/vecteezy_ai-robot-technology-concept_22242520_82/vecteezy_ai-robot-technology-concept_22242520-1.jpg";
+import aiBg from "./assets/vecteezy_dynamic-flowing-wavy-abstract-light-line-vector-in-blue_24054189_373/vecteezy_dynamic-flowing-wavy-abstract-light-line-vector-in-blue_24054189-1.jpg";
 
 const AIDS = () => {
   return (
     <>
-      <div id="aids" className=" m-4 p-4">
+      <div id="aids" className=" p-4 container">
         <h1 className="text-4xl font-bold">AI and Data science</h1>
-        
+        <div className="row1 flex flex-col lg:flex-row mt-4 rounded-3xl">
+          <img className=" rounded-3xl" src={aiBg} alt="" />
+          <div className=" absolute p-4 text-white">
+            <h1 className=" text-4xl font-bold md:mt-10">
+              WORK SMARTER AND FASTER
+            </h1>
+          </div>
+        </div>
       </div>
     </>
   );
--- client/src/pages/DesignCreation.jsx
+++ client/src/pages/DesignCreation.jsx
@@ -1,14 +1,5 @@
 import Navebar from "../components/Navebar";
 
 export default function DesignCreation() {
-  return (
-    <>
-      <div
-        id="design"
-        className=" h-screen w-screen bg-blue-200 flex justify-center items-center"
-      >
-        <h1 className=" text-4xl font-medium">Working on it! 👨🏻‍💻</h1>
-      </div>
-    </>
-  );
+  return <></>;
 }
